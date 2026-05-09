5e7dd343bf784046589604e45fa26bebbd2314ea
Adetunji Adebobola <bob@Adetunjis-MacBook-Pro.local>
2026-05-09 04:14:02 +0100

    Deploy: fix carousel image paths for static export (main 3489627)

--- _next/static/chunks/6247b2a53d6c8174.js
+++ _next/static/chunks/6247b2a53d6c8174.js
@@ -59,4 +59,4 @@
           aspect-ratio: 16 / 10 !important;
         }
       }
-    `})}e.s(["default",()=>R],62522)},82800,e=>{e.v(t=>Promise.all(["static/chunks/969b27dc7f150136.css","static/chunks/1fe946a0be6f632c.js","static/chunks/258e73c44f6ebcc4.js"].map(t=>e.l(t))).then(()=>t(88834)))}]);
+    `})}e.s(["default",()=>R],62522)},82800,e=>{e.v(t=>Promise.all(["static/chunks/969b27dc7f150136.css","static/chunks/1fe946a0be6f632c.js","static/chunks/0b317c2a5a87761d.js"].map(t=>e.l(t))).then(()=>t(88834)))}]);
